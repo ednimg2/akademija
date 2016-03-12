384f7217cf8348dd75a632c63101941c0b56a728
Mindaugas Galvanauskas <m.galvanauskas@gmail.com>
2016-03-12 19:17:32 +0200

    Homework 2 Show city weather from yahoo weather api

---
+++ src/Nfq/WeatherBundle/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Nfq\WeatherBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use GuzzleHttp\Client;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/weather/{city}")
+     */
+    public function indexAction($city)
+    {
+        $client = new Client();
+        $base_url = "https://query.yahooapis.com/v1/public/yql";
+        $yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='".$city."')  and u='c'";
+        $yql_query_url = $base_url . "?q=" . urlencode($yql_query) . "&format=json";
+
+        $res = $client->request('GET', $yql_query_url);
+
+        $body = json_decode($res->getBody(true));
+        //echo"<pre>";print_r( $body->query->results->channel );echo"</pre>";
+        $item = $body->query->results->channel;
+        $params = [
+            'title' => $item->title,
+            'temp' => $item->item->condition->temp,
+            'units' => $item->units
+        ];
+
+        return $this->render('NfqWeatherBundle:Default:index.html.twig', $params);
+    }
+}
